ee1786464a964d4d130e8d86c6787bd08675a87e
MattiaMessana <mattia.messana@icloud.com>
2024-04-29 18:49:50 +0200

    try bonus

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-        curIndex: 0,
         mailsList: [],
-
+        isLoading: false
+        
       }
     },
     methods: {
       
          generateMails: function() {
+            this.isLoading = true;
             for(let i = 0 ; i < 10; i++) {
-                axios
-                .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-                .then((resp) => { 
-                    console.log(resp.data.response);
-                    this.mailsList.push(resp.data.response);
-                })
+                    axios
+                    .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                    .then((resp) => { 
+                        console.log(resp.data.response);
+                        this.mailsList.push(resp.data.response);
+                        this.isLoading = false;
+                    })
+                    this.mailsList = [];
             }
-         }
-
+         },
     },
   }).mount('#app')
